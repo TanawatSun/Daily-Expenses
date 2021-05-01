5fdec824506bff7019e3c1b01ee95d152f2bf980
TanawatSun <sun__sun@windowslive.com>
2021-05-01 15:12:51 +0700

    fix expense

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const DUMMY_Expense = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "New PS5",
+    amount: 499.99,
+    date: new Date(2021, 2, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New Monitor", amount: 199.99, date: new Date(2021, 1, 1) },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: "New Game",
+    amount: 79.99,
+    date: new Date(2021, 3, 9),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: "New Fan",
+    amount: 20,
+    date: new Date(2021, 3, 19),
   },
 ];
 
